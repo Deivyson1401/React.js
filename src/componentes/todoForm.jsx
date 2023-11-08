db90b23beb36f77e9765f24196e5799f7af43038
Deivyson José da Silva <139878479+Deivyson1401@users.noreply.github.com>
2023-11-08 07:49:48 +0000

    Adicionado função de limpar imput e option

--- src/componentes/todoForm.jsx
+++ src/componentes/todoForm.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
+import { useState } from 'react'
 
 const TodoForm = ()=> {
+    const [value, setValue] = useState()
+    const [category, setCategory] = useState()
+
+    const handlesubmit = (e) => {
+        e.preventDefault();
+        if (!value || !category) return;
+
+        setCategory("")
+        setValue("")
+
+    }
+
     return (
         <div className='todo-form'>
             <h2>Criar tarefa:</h2>
-            <form >
-                <input type="text" placeholder='digite o título'/>
-                <select>
+            <form onSubmit={handlesubmit}>
+                <input value={value} type="text" placeholder='digite o título' onChange={(e) => setValue(e.target.value)}/>
+                <select value={category} onChange={(e) => setCategory(e.target.value)}>
                     <option value="">Selecione uma categoria</option>
                     <option value="Trabalho">Trabalho</option>
                     <option value="Pessoal">Pessoal</option>
